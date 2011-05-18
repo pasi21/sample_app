16aaa52e386729e3e38d7bbc9fa9f2b65ae15320
Pascal Koehn <passi-kohn@web.de>
2011-05-19 00:02:58 +0200

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,8 @@ class User < ActiveRecord::Base
 
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    user && user.has_password?(submitted_password) ? user : nil
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
   end
 
   private
